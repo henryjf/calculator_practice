c2387e0d8621b8a54e7f2f5619c9b2f443c3eab1
JEFF HENRY <henry_jf@hotmail.com>
2016-03-18 07:16:38 -0400

    initialize

---
+++ main.js
@@ -0,0 +1,21 @@
+// / * When I click the "calculate" button, the values in the
+//  // first box should be added, subtracted, multiplied or divided 
+//  // by the value in the second box. 
+//  // * You'll need to convert the values to a number 
+//  // (think `Number(value)`) in order to process them.
+// // * Your output should be injected into the answer element
+
+//declare variables (there are four of them)
+
+var = num1, num2, button, answer
+
+//elements
+
+num1 = document.querySelector('num_1');
+num2 = document.querySelector('num_2');
+button = document.querySelector('calculateBtn');
+answer = document.querySelector('answer');
+
+//do the math
+
+// get the answer
